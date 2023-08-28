6e11435820a295f1cdc233fbe69274d847de77a4
HoChanLee <106670454+HoChanLee@users.noreply.github.com>
2023-08-28 16:40:43 +0900

    Init : 프로젝트 셍성

---
+++ src/test/java/zerobase/EasyRemittance/EasyRemittanceApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.EasyRemittance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyRemittanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
